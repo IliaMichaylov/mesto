d907e9d8d6bc704a68c64a742db08f13264c2828
Ilia <masterlodokufa@yandex.ru>
2023-05-16 22:13:56 +0500

    16.05.23

---
+++ index.js
@@ -0,0 +1,26 @@
+const openPopupButton = document.querySelector("#open-popup-button");
+const closePopupButton = document.querySelector("#close-popup-button");
+const editPopup = document.querySelector("#edit-popup");
+const profileTitle = document.querySelector(".profile__title");
+const profileSubtitle = document.querySelector(".profile__subtitle");
+const nameInput = document.querySelector("#name-input");
+const subnameInput = document.querySelector("#subname-input");
+const editForm = document.querySelector("#edit-form");
+
+openPopupButton.addEventListener("click", function () {
+  editPopup.classList.add("popup_is-opened");
+});
+closePopupButton.addEventListener("click", function () {
+  editPopup.classList.remove("popup_is-opened");
+});
+
+nameInput.value = profileTitle.textContent;
+subnameInput.value = profileSubtitle.textContent;
+
+
+editForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = subnameInput.value;
+});
