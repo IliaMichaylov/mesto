0dfe8a6b6d8036351e4001533e897bd9e2d4112b
Ilia <masterlodokufa@yandex.ru>
2023-08-23 20:59:53 +0500

    23.08.23-2

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,43 @@
-const openPopupButton = document.querySelector("#open-popup-button");
-const openPopupButtonAddCard = document.querySelector("#open-pop-up-button");
-const closePopupButton = document.querySelector("#close-popup-button");
-const closePopupButtonAddCard = document.querySelector("#close-pop-up-button");
-const editPopup = document.querySelector("#edit-popup");
-const cardPopup = document.querySelector("#card-pop-up");
+/**
+ * Попап-окно
+*/
+function closePopup() {
+  const popup = document.querySelector('.popup_opened');
+  popup.classList.remove("popup_opened");
+
+  const closePopupButton = popup.querySelector("#close-popup-button");
+  closePopupButton.removeEventListener("click");
+}
+
+function openPopup(id = '') {
+  const popup = document.querySelector(`#${id}`);
+  popup.classList.add("popup_opened");
+
+  const closePopupButton = popup.querySelector("#close-popup-button");
+  closePopupButton.addEventListener("click", closePopup);
+}
+
+/**
+ * Редактирование профиля
+*/
+const openEditProfileFormButton = document.querySelector("#open-edit-profile-form-button");
+openEditProfileFormButton.addEventListener("click", function () {
+  prepareFormFields()
+  openPopup('edit-profile-popup')
+});
 
 const profileTitle = document.querySelector(".profile__title");
-
 const profileSubtitle = document.querySelector(".profile__subtitle");
-const nominationInput = document.querySelector("#nomination-input");
-const nameInput = document.querySelector("#name-input");
-const subnameInput = document.querySelector("#subname-input");
-const urlInput = document.querySelector("#url-input");
-const cardForm = document.querySelector("#card-form");
-const editForm = document.querySelector("#edit-form");
 
-function closePopup() {
-  editPopup.classList.remove("popup_opened");
-  cardPopup.classList.remove("pop-up_opened");
-}
+function prepareFormFields() {
+  const nameInput = document.querySelector("#name-input");
+  const subnameInput = document.querySelector("#subname-input");
 
-function openPopup() {
   nameInput.value = profileTitle.textContent;
   subnameInput.value = profileSubtitle.textContent;
-  editPopup.classList.add("popup_opened");
-  cardPopup.classList.remove("pop-up_opened");
 }
 
-openPopupButton.addEventListener("click", openPopup);
-closePopupButton.addEventListener("click", closePopup);
-
+const editForm = document.querySelector("#edit-form");
 editForm.addEventListener("submit", function (event) {
   event.preventDefault();
 
@@ -38,3 +46,74 @@ editForm.addEventListener("submit", function (event) {
   closePopup();
 });
 
+/**
+ * Вывод карточек
+*/
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+function initCards() {
+  const cardsList = document.querySelector('.elements__cell-list')
+
+  initialCards.forEach(function(card) {
+    const element = document.createElement('li')
+    element.classList.add('elements__cell')
+  
+    element.innerHTML = `
+      <img
+        src="${card.link}"
+        alt="${card.name}"
+        class="elements__cell-img"
+      />
+      <div class="elements__cell-name">
+        <h2 class="elements__cell-title">${card.name}</h2>
+        <button type="button" class="elements__cell-heart"></button>
+      </div>
+    `
+
+    cardsList.appendChild(element)
+  })
+}
+
+initCards();
+
+/**
+ * Добавление карточки
+ */
+const openAddCardFormButton = document.querySelector("#open-add-card-form-button");
+openAddCardFormButton.addEventListener("click", function () {
+  openPopup('add-card-popup')
+});
+
+const addCardForm = document.querySelector("#add-card-form");
+addCardForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = subnameInput.value;
+  closePopup();
+});
